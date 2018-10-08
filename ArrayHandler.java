51c86273a03eafcca5a10ab7b8bd3668ac95bec6
Kirill <nosolution01@gmail.com>
2018-10-09 00:00:58 +0400

    - Removed excessive console output - Refactored variables, classes and methods names(according to JNC) -Filling array with random numbers moved to a separate method

--- ArrayHandler.java
+++ ArrayHandler.java
@@ -1,21 +1,37 @@
-public class ArrayHandler {
-    private int LargestNegativeNumber = -10, LargestNegativeNumberIndex;
-    private int LesserPositiveNumber = 10, LesserPositiveNumberIndex;
+/*
+ * ArrayHandler class
+ * Contains main functionality for solving few operations with arrays
+ * EPAM Java training for automation QA
+ * Author: Kirill Kuzin
+ * Lesson 1 Task 1
+ * */
 
-    public void DoSwap(int[] ArrayIn) { // Method that swaps biggest negative and lesser positive numbers in the input array
-        System.out.print("\nFirst Task: Swap biggest negative and lesser positive numbers:");
+import java.lang.Math; // for Math.random();
+
+public class ArrayHandler {
+    private int largestNegativeNumber = -10, largestNegativeNumberIndex;
+    private int lesserPositiveNumber = 10, lesserPositiveNumberIndex;
+    public int[] doFillArray(int arraySize){
+        int[] targetArray = new int[arraySize];
+        for (int j = 0; j < targetArray.length; j++) { // Filling JunkArray with random numbers from [-10;10]
+            targetArray[j] = (int)((Math.random()*21)-10);
+        }
+        return(targetArray);
+    }
+    public void doSwap(int[] ArrayIn) { // Swaps biggest negative and lesser positive numbers in the input array
+        System.out.print("\nFirst Task:");
         for (int i = 0; i < ArrayIn.length; i++) {
-            if (ArrayIn[i] < 0 && ArrayIn[i] > LargestNegativeNumber) {
-                LargestNegativeNumber = ArrayIn[i];
-                LargestNegativeNumberIndex = i;
+            if (ArrayIn[i] < 0 && ArrayIn[i] > largestNegativeNumber) {
+                largestNegativeNumber = ArrayIn[i];
+                largestNegativeNumberIndex = i;
             }
-            if (ArrayIn[i] > 0 && ArrayIn[i] < LesserPositiveNumber) {
-                LesserPositiveNumber = ArrayIn[i];
-                LesserPositiveNumberIndex = i;
+            if (ArrayIn[i] > 0 && ArrayIn[i] < lesserPositiveNumber) {
+                lesserPositiveNumber = ArrayIn[i];
+                lesserPositiveNumberIndex = i;
             }
         }
-        ArrayIn[LesserPositiveNumberIndex] = LargestNegativeNumber;
-        ArrayIn[LargestNegativeNumberIndex] = LesserPositiveNumber;
+        ArrayIn[lesserPositiveNumberIndex] = largestNegativeNumber;
+        ArrayIn[largestNegativeNumberIndex] = lesserPositiveNumber;
         System.out.println();
         for (int k = 0; k < ArrayIn.length; k++) {
             System.out.print(ArrayIn[k] + "  ");
@@ -24,8 +40,8 @@ public class ArrayHandler {
 
     private int Sum;
 
-    public void DoSum(int ArrayIn[]) {
-        System.out.println("\nSecond Task: Sum of numbers on even positions");
+    public void doSum(int ArrayIn[]) { //Sum of numbers on even positions
+        System.out.println("\nSecond Task:");
         for (int i = 0; i < ArrayIn.length; i++) {
             if ((i + 1) % 2 == 0) {
                 Sum = Sum + ArrayIn[i];
@@ -34,8 +50,8 @@ public class ArrayHandler {
         System.out.print(Sum);
     }
 
-    public void DoReplace(int ArrayIn[]) {
-        System.out.println("\nThird Task: Replaace all negative numbers in array with zeros");
+    public void doReplace(int ArrayIn[]) {
+        System.out.println("\nThird Task:"); //Replaces all negative numbers in array with zeros
         for (int i = 0; i < ArrayIn.length; i++) {
             if (ArrayIn[i] < 0) {
                 ArrayIn[i] = 0;
@@ -45,8 +61,8 @@ public class ArrayHandler {
 
     }
 
-    public void DoTriple(int[] ArrayIn) {
-        System.out.println("\nForth Task: Triple the positive numbers in array if there are negative number in the next position");
+    public void doTriple(int[] ArrayIn) {
+        System.out.println("\nForth Task:"); //Triple the positive numbers in array if there are negative number in the next position
         for (int i = 0; i < ArrayIn.length - 1; i++) {
             if (ArrayIn[i] > 0 && ArrayIn[i + 1] < 0) {
                 ArrayIn[i] = ArrayIn[i] * 3;
@@ -55,8 +71,8 @@ public class ArrayHandler {
         }
     }
 
-    public void DoDiff(int[] ArrayIn) {
-        System.out.println("\nFifth Task: Difference between array average and lesser number in array:");
+    public void doDiff(int[] ArrayIn) {
+        System.out.println("\nFifth Task:"); //Difference between array average and lesser number in array
         int Average, Sum = 0;
         int Lesser = ArrayIn[0];
         for (int i = 0; i < ArrayIn.length; i++) {
@@ -69,8 +85,8 @@ public class ArrayHandler {
         System.out.print(Average + " - " + Lesser + " = " + (Average - Lesser));
     }
 
-    public void DoCheck(int[] ArrayIn) {
-        System.out.println("\nSixth Task: Return repeated array elements on uneven positions:");
+    public void doCheck(int[] ArrayIn) { //Returns repeated array elements on uneven positions
+        System.out.println("\nSixth Task:");
         int count;
         for (int i = 0; i < ArrayIn.length; i++) {
             count = 0;
--- Main.java
+++ Main.java
@@ -1,21 +1,26 @@
-import java.lang.Math; // for Math.random();
+/*
+ * Main class
+ * EPAM Java training for automation QA
+ * Author: Kirill Kuzin
+ * Lesson 1 Task 1
+ * */
 
 
 public class Main {
     public static void main(String[] args) {
-        int[] JunkArray = new int[20];
+        ArrayHandler arrayHandler = new ArrayHandler();
+        int[] junkArray = arrayHandler.doFillArray(20);
         System.out.println("Target array filled with random numbers:");
-        for (int j = 0; j < JunkArray.length; j++) { // Filling JunkArray with random numbers from [-10;10]
-            JunkArray[j] = (int)((Math.random()*21)-10);
-            System.out.print(JunkArray[j]+"  ");
+        for (int j = 0; j < junkArray.length; j++) { // Filling JunkArray with random numbers from [-10;10]
+            System.out.print(junkArray[j]+"  ");
         }
 
-        ArrayHandler arrayHandler = new ArrayHandler();
-        arrayHandler.DoSwap(JunkArray.clone());
-        arrayHandler.DoSum(JunkArray.clone());
-        arrayHandler.DoReplace(JunkArray.clone());
-        arrayHandler.DoTriple(JunkArray.clone());
-        arrayHandler.DoDiff(JunkArray.clone());
-        arrayHandler.DoCheck(JunkArray.clone());
+
+        arrayHandler.doSwap(junkArray.clone());
+        arrayHandler.doSum(junkArray.clone());
+        arrayHandler.doReplace(junkArray.clone());
+        arrayHandler.doTriple(junkArray.clone());
+        arrayHandler.doDiff(junkArray.clone());
+        arrayHandler.doCheck(junkArray.clone());
     }
 }
